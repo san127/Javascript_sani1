3cc85c6ac5c18118f24eba7ef8521b0657c3ebeb
san127 <121002157+san127@users.noreply.github.com>
2023-12-14 23:09:40 +0530

    boink

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("Hellow")
+
+const accountId = 123456
+let accountEmail = "bunny@google.co"
+var accountPassword = "1234"
+accounrCity = "Mapusa"
+
+//accountId = 2
+
+console.log(accountEmail);
